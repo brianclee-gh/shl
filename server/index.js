29bc888c4d492abf937fab81b141e9dc5ec430dc
Brian <blee0613@gmail.com>
2021-11-08 00:33:42 -0800

    server skeleton

--- server/index.js
+++ server/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const morgan = require('morgan');
+const router = require('./routes');
 
 const app = express();
 app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+module.exports.app = app;
 
 const PORT = 3000;
+app.set('port', PORT);
+app.use('/', router);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`)
